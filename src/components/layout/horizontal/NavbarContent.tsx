0cd888888f05d3ccb88c13aefd00a587b7e69816
Sina Hajipour <dev.sinahajipour@gmail.com>
2024-05-30 10:13:13 +0330

    add setting shortcut

--- src/components/layout/horizontal/NavbarContent.tsx
+++ src/components/layout/horizontal/NavbarContent.tsx
@@ -13,24 +13,68 @@ import UserDropdown from '@components/layout/shared/UserDropdown'
 import useHorizontalNav from '@menu/hooks/useHorizontalNav'
 
 // Util Imports
-import {horizontalLayoutClasses} from '@layouts/utils/layoutClasses'
+import ShortcutsDropdown, { ShortcutsType } from '@components/layout/shared/ShortcutsDropdown'
+import { horizontalLayoutClasses } from '@layouts/utils/layoutClasses'
+
+
+// Vars
+const shortcuts: ShortcutsType[] = [
+    {
+        url: '/apps/calendar',
+        icon: 'tabler-calendar',
+        title: 'Calendar',
+        subtitle: 'Appointments'
+    },
+    {
+        url: '/apps/invoice/list',
+        icon: 'tabler-file-dollar',
+        title: 'Invoice App',
+        subtitle: 'Manage Accounts'
+    },
+    {
+        url: '/apps/user/list',
+        icon: 'tabler-user',
+        title: 'Users',
+        subtitle: 'Manage Users'
+    },
+    {
+        url: '/apps/roles',
+        icon: 'tabler-users-group',
+        title: 'Role Management',
+        subtitle: 'Permissions'
+    },
+    {
+        url: '/',
+        icon: 'tabler-device-desktop-analytics',
+        title: 'Dashboard',
+        subtitle: 'User Dashboard'
+    },
+    {
+        url: '/pages/account-settings',
+        icon: 'tabler-settings',
+        title: 'Settings',
+        subtitle: 'Account Settings'
+    }
+]
+
 
 const NavbarContent = () => {
     // Hooks
-    const {isBreakpointReached} = useHorizontalNav()
+    const { isBreakpointReached } = useHorizontalNav()
 
     return (
         <div
             className={classnames(horizontalLayoutClasses.navbarContent, 'flex items-center justify-between gap-4 is-full')}
         >
             <div className='flex items-center gap-4'>
-                <NavToggle/>
+                <NavToggle />
                 {/* Hide Logo on Smaller screens */}
-                {!isBreakpointReached && <Logo/>}
+                {!isBreakpointReached && <Logo />}
             </div>
             <div className='flex items-center'>
-                <ModeDropdown/>
-                <UserDropdown/>
+                <ModeDropdown />
+                <ShortcutsDropdown shortcuts={shortcuts} />
+                <UserDropdown />
             </div>
         </div>
     )
--- src/components/layout/vertical/NavbarContent.tsx
+++ src/components/layout/vertical/NavbarContent.tsx
@@ -10,19 +10,33 @@ import UserDropdown from '@components/layout/shared/UserDropdown'
 
 // Util Imports
 import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
+import ShortcutsDropdown, { ShortcutsType } from '../shared/ShortcutsDropdown'
+
+
+// Vars
+const shortcuts: ShortcutsType[] = [
+    {
+        url: '/admin/membership/admins',
+        icon: 'tabler-calendar',
+        title: 'Calendar',
+        subtitle: 'Appointments'
+    },
+]
+
 
 const NavbarContent = () => {
-  return (
-    <div className={classnames(verticalLayoutClasses.navbarContent, 'flex items-center justify-between gap-4 is-full')}>
-      <div className='flex items-center gap-4'>
-        <NavToggle />
-        <ModeDropdown />
-      </div>
-      <div className='flex items-center'>
-        <UserDropdown />
-      </div>
-    </div>
-  )
+    return (
+        <div className={classnames(verticalLayoutClasses.navbarContent, 'flex items-center justify-between gap-4 is-full')}>
+            <div className='flex items-center gap-4'>
+                <NavToggle />
+                <ModeDropdown />
+            </div>
+            <div className='flex items-center'>
+                <ShortcutsDropdown shortcuts={shortcuts} />
+                <UserDropdown />
+            </div>
+        </div>
+    )
 }
 
 export default NavbarContent
