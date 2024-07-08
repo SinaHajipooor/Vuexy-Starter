cce0c8264cf96061ed8a50e9b4e518e1b1081575
Sina Hajipour <dev.sinahajipour@gmail.com>
2024-07-08 20:50:12 +0330

    fix root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import 'react-perfect-scrollbar/dist/css/styles.css'
 
 // Type Imports
-import type {ChildrenType} from '@core/types'
+import type { ChildrenType } from '@core/types'
 
 // Style Imports
 import '@/app/globals.css'
@@ -18,14 +18,14 @@ export const metadata = {
         'Vuexy - MUI Next.js Admin Dashboard Template - is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.'
 }
 
-const RootLayout = ({children}: ChildrenType) => {
+const RootLayout = ({ children }: ChildrenType) => {
     // Vars
     const direction = 'rtl';
 
     return (
         <html id='__next' lang='en' dir={direction}>
-            <Loading/>
-        <body className='flex is-full min-bs-full flex-auto flex-col'>{children}</body>
+            <Loading />
+            <body className='flex is-full min-bs-full flex-auto flex-col'>{children}</body>
         </html>
     )
 }
