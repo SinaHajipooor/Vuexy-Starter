d6aad9fadf63b5e652a30e177811c7d72600cf40
Sina Hajipour <dev.sinahajipour@gmail.com>
2024-07-08 18:42:01 +0330

    fix user dropdown badge

--- src/components/layout/shared/UserDropdown.tsx
+++ src/components/layout/shared/UserDropdown.tsx
@@ -26,122 +26,122 @@ import { useSettings } from '@core/hooks/useSettings'
 
 // Styled component for badge content
 const BadgeContentSpan = styled('span')({
-  width: 8,
-  height: 8,
-  borderRadius: '50%',
-  cursor: 'pointer',
-  backgroundColor: 'var(--mui-palette-success-main)',
-  boxShadow: '0 0 0 2px var(--mui-palette-background-paper)'
+    width: 8,
+    height: 8,
+    borderRadius: '50%',
+    cursor: 'pointer',
+    backgroundColor: 'var(--mui-palette-success-main)',
+    boxShadow: '0 0 0 2px var(--mui-palette-background-paper)'
 })
 
 const UserDropdown = () => {
-  // States
-  const [open, setOpen] = useState(false)
+    // States
+    const [open, setOpen] = useState(false)
 
-  // Refs
-  const anchorRef = useRef<HTMLDivElement>(null)
+    // Refs
+    const anchorRef = useRef<HTMLDivElement>(null)
 
-  // Hooks
-  const router = useRouter()
+    // Hooks
+    const router = useRouter()
 
-  const { settings } = useSettings()
+    const { settings } = useSettings()
 
-  const handleDropdownOpen = () => {
-    !open ? setOpen(true) : setOpen(false)
-  }
-
-  const handleDropdownClose = (event?: MouseEvent<HTMLLIElement> | (MouseEvent | TouchEvent), url?: string) => {
-    if (url) {
-      router.push(url)
+    const handleDropdownOpen = () => {
+        !open ? setOpen(true) : setOpen(false)
     }
 
-    if (anchorRef.current && anchorRef.current.contains(event?.target as HTMLElement)) {
-      return
+    const handleDropdownClose = (event?: MouseEvent<HTMLLIElement> | (MouseEvent | TouchEvent), url?: string) => {
+        if (url) {
+            router.push(url)
+        }
+
+        if (anchorRef.current && anchorRef.current.contains(event?.target as HTMLElement)) {
+            return
+        }
+
+        setOpen(false)
     }
 
-    setOpen(false)
-  }
+    const handleUserLogout = async () => {
+        // Redirect to login page
+        router.push('/login')
+    }
 
-  const handleUserLogout = async () => {
-    // Redirect to login page
-    router.push('/login')
-  }
-
-  return (
-    <>
-      <Badge
-        ref={anchorRef}
-        overlap='circular'
-        badgeContent={<BadgeContentSpan onClick={handleDropdownOpen} />}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        className='mis-2'
-      >
-        <Avatar
-          ref={anchorRef}
-          alt='John Doe'
-          src='/images/avatars/1.png'
-          onClick={handleDropdownOpen}
-          className='cursor-pointer bs-[38px] is-[38px]'
-        />
-      </Badge>
-      <Popper
-        open={open}
-        transition
-        disablePortal
-        placement='bottom-end'
-        anchorEl={anchorRef.current}
-        className='min-is-[240px] !mbs-3 z-[1]'
-      >
-        {({ TransitionProps, placement }) => (
-          <Fade
-            {...TransitionProps}
-            style={{
-              transformOrigin: placement === 'bottom-end' ? 'right top' : 'left top'
-            }}
-          >
-            <Paper className={settings.skin === 'bordered' ? 'border shadow-none' : 'shadow-lg'}>
-              <ClickAwayListener onClickAway={e => handleDropdownClose(e as MouseEvent | TouchEvent)}>
-                <MenuList>
-                  <div className='flex items-center plb-2 pli-6 gap-2' tabIndex={-1}>
-                    <Avatar alt='John Doe' src='/images/avatars/1.png' />
-                    <div className='flex items-start flex-col'>
-                      <Typography className='font-medium' color='text.primary'>
-                        سیناحاجی پور
-                      </Typography>
-                      <Typography fontSize={12} variant='caption'>مدیرسیستم</Typography>
-                    </div>
-                  </div>
-                  <Divider className='mlb-1' />
-                  <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e)}>
-                    <i className='tabler-user text-[22px]' />
-                    <Typography variant='caption' color='text.primary'>اطلاعات کاربری</Typography>
-                  </MenuItem>
-                  <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e)}>
-                    <i className='tabler-settings text-[22px]' />
-                    <Typography variant='caption' color='text.primary'>تنظیمات</Typography>
-                  </MenuItem>
-
-                  <div className='flex items-center plb-2 pli-3'>
-                    <Button
-                      fullWidth
-                      variant='contained'
-                      color='error'
-                      size='small'
-                      endIcon={<i className='tabler-logout' />}
-                      onClick={handleUserLogout}
-                      sx={{ '& .MuiButton-endIcon': { marginInlineStart: 1.5 } }}
+    return (
+        <>
+            <Badge
+                ref={anchorRef}
+                overlap='circular'
+                badgeContent={<BadgeContentSpan onClick={handleDropdownOpen} />}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+                className='mis-2'
+            >
+                <Avatar
+                    ref={anchorRef}
+                    alt='John Doe'
+                    src='/images/avatars/1.png'
+                    onClick={handleDropdownOpen}
+                    className='cursor-pointer bs-[38px] is-[38px]'
+                />
+            </Badge>
+            <Popper
+                open={open}
+                transition
+                disablePortal
+                placement='bottom-end'
+                anchorEl={anchorRef.current}
+                className='min-is-[240px] !mbs-3 z-[1]'
+            >
+                {({ TransitionProps, placement }) => (
+                    <Fade
+                        {...TransitionProps}
+                        style={{
+                            transformOrigin: placement === 'bottom-end' ? 'right top' : 'left top'
+                        }}
                     >
-                      خروج
-                    </Button>
-                  </div>
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Fade>
-        )}
-      </Popper>
-    </>
-  )
+                        <Paper className={settings.skin === 'bordered' ? 'border shadow-none' : 'shadow-lg'}>
+                            <ClickAwayListener onClickAway={e => handleDropdownClose(e as MouseEvent | TouchEvent)}>
+                                <MenuList>
+                                    <div className='flex items-center plb-2 pli-6 gap-2' tabIndex={-1}>
+                                        <Avatar alt='John Doe' src='/images/avatars/1.png' />
+                                        <div className='flex items-start flex-col'>
+                                            <Typography className='font-medium' color='text.primary'>
+                                                سیناحاجی پور
+                                            </Typography>
+                                            <Typography fontSize={12} variant='caption'>مدیرسیستم</Typography>
+                                        </div>
+                                    </div>
+                                    <Divider className='mlb-1' />
+                                    <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e)}>
+                                        <i className='tabler-user text-[22px]' />
+                                        <Typography variant='caption' color='text.primary'>اطلاعات کاربری</Typography>
+                                    </MenuItem>
+                                    <MenuItem className='mli-2 gap-3' onClick={e => handleDropdownClose(e)}>
+                                        <i className='tabler-settings text-[22px]' />
+                                        <Typography variant='caption' color='text.primary'>تنظیمات</Typography>
+                                    </MenuItem>
+
+                                    <div className='flex items-center plb-2 pli-3'>
+                                        <Button
+                                            fullWidth
+                                            variant='contained'
+                                            color='error'
+                                            size='small'
+                                            endIcon={<i className='tabler-logout' />}
+                                            onClick={handleUserLogout}
+                                            sx={{ '& .MuiButton-endIcon': { marginInlineStart: 1.5 } }}
+                                        >
+                                            خروج
+                                        </Button>
+                                    </div>
+                                </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Fade>
+                )}
+            </Popper>
+        </>
+    )
 }
 
 export default UserDropdown
