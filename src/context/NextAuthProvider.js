1699245f8ad5abe2c3b5c066c5ba45b283c00377
Sina Hajipour <dev.sinahajipour@gmail.com>
2024-07-09 10:49:40 +0330

    add next auth session provider

---
+++ src/context/NextAuthProvider.js
@@ -0,0 +1,9 @@
+import { SessionProvider } from "next-auth/react"
+
+function NextAuthProvider({ children }) {
+    return (
+        <SessionProvider>{children}</SessionProvider>
+    )
+}
+
+export default NextAuthProvider
